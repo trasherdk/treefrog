af592503500540ff0e4309cc9ff414f1d8e18ac7
Gus Hogg-Blake <gus@treefrog-editor.com>
2021-06-11 13:51:22 +0100

    wip

--- src/components/Editor/modeSwitchKey.js
+++ src/components/Editor/modeSwitchKey.js
@@ -1,15 +1,10 @@
 module.exports = function(editor) {
 	let keyIsDown = false;
 	let keyDownAt;
-	let justSwitchedToNormalMode = false;
 	
 	return {
 		keydown(e) {
-			console.log("mode switch keydown");
-			
 			if (keyIsDown) {
-				console.log("key already down");
-				
 				return;
 			}
 			
@@ -17,37 +12,23 @@ module.exports = function(editor) {
 			keyDownAt = Date.now();
 			
 			if (editor.mode === "ast") {
-				console.log("switch to normal mode");
-				
 				editor.switchToNormalMode();
-				
-				justSwitchedToNormalMode = true;
 			} else {
-				console.log("switch to ast mode");
-				
 				editor.switchToAstMode(true);
 			}
 		},
 		
 		keyup(e) {
-			console.log("mode switch keyup");
-			
 			let downTime = Date.now() - keyDownAt;
 			
 			keyIsDown = false;
 			
 			if (editor.mode === "ast") {
 				if (downTime >= editor.minHoldTime) {
-					console.log("switch to normal mode");
-					
 					editor.switchToNormalMode();
 				} else {
-					console.log("switch to ast mode");
-					
 					editor.switchToAstMode(false);
 				}
-			} else {
-				console.log("not in ast mode");
 			}
 		},
 	};
