d7dd98f41a05a380889f4cc5fd78a344dc8a7ded
Gus Hogg-Blake <gus@treefrog-editor.com>
2021-07-01 11:59:28 +0100

    wip

--- src/modules/langs/js/codeIntel/astSelection/fromLineIndex.js
+++ src/modules/langs/js/codeIntel/astSelection/fromLineIndex.js
@@ -13,14 +13,8 @@ if a header, then to the immediate footer
 expanding goes to next indent level, NOT the entire ladder - there's a diff command
 for that (like {} for next/prev paragraph in vim)
 
-cutting selection goes from first opener to that opener's closer in the footer,
-if present (otherwise ?)
-
-^ special cases for easy if statements/function headers?  ) { and ) => always indicate
-function header (or if/while/for in the case of ) {).  } else is always an else or
-else if statement
-
-expand could go to ladder actually - seems poss more useful, and not unintuitive
+cutting selections where the header or footer is a header-footer (e.g. } else {)
+- to be handled intelligently by the lang
 */
 
 function fromLineIndex(lines, lineIndex, forHilite) {
