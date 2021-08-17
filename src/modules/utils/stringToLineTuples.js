76918d72612ee9d4d3602493682000220d42fee5
Gus Hogg-Blake <gus@treefrog-editor.com>
2021-08-17 15:06:09 +0100

    wip

--- src/modules/utils/stringToLineTuples.js
+++ src/modules/utils/stringToLineTuples.js
@@ -3,9 +3,9 @@ let guessIndent = require("modules/utils/guessIndent");
 let getIndentLevel = require("modules/utils/getIndentLevel");
 let getIndentationDetails = require("modules/utils/getIndentationDetails");
 
-module.exports = function(str) {
+module.exports = function(str, defaultIndent="\t") {
 	let lines = str.split(getNewline(str));
-	let indentationDetails = getIndentationDetails(guessIndent(str));
+	let indentationDetails = getIndentationDetails(guessIndent(str) || defaultIndent);
 	
 	return lines.map(function(line) {
 		return [
