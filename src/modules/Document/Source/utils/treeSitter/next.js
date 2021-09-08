b456a28d86e91bc6f9efa9fc6f083c842bebaa2f
Gus Hogg-Blake <gus@treefrog-editor.com>
2021-09-08 10:56:49 +0100

    skip empty nodes in next/prev to avoid tree-sitter parent bug

--- src/modules/Document/Source/utils/treeSitter/next.js
+++ src/modules/Document/Source/utils/treeSitter/next.js
@@ -1,15 +1,74 @@
-module.exports = function(node) {
-	if (node.firstChild) {
-		return node.firstChild;
+//module.exports = function(node) {
+//	if (node.firstChild) {
+//		return node.firstChild;
+//	}
+//	
+//	if (node.nextSibling) {
+//		return node.nextSibling;
+//	}
+//	
+//	while (node = node.parent) {
+//		if (node.nextSibling) {
+//			return node.nextSibling;
+//		}
+//	}
+//	
+//	return null;
+//}
+
+// NOTE tree-sitter has a bug where zero-length nodes don't have
+// the right parent, so we skip them
+
+function getFirstChild(node) {
+	for (let child of node.children) {
+		if (child.text.length > 0) {
+			return child;
+		}
 	}
 	
-	if (node.nextSibling) {
-		return node.nextSibling;
+	return null;
+}
+
+function getNextSibling(node) {
+	let {parent} = node;
+	
+	if (!parent) {
+		return null;
+	}
+	
+	let foundNode = false;
+	
+	for (let child of parent.children) {
+		if (foundNode && child.text.length > 0) {
+			return child;
+		}
+		
+		if (child.id === node.id) {
+			foundNode = true;
+		}
+	}
+	
+	return null;
+}
+
+module.exports = function(node) {
+	let firstChild = getFirstChild(node);
+	
+	if (firstChild) {
+		return firstChild;
+	}
+	
+	let nextSibling = getNextSibling(node);
+	
+	if (nextSibling) {
+		return nextSibling;
 	}
 	
 	while (node = node.parent) {
-		if (node.nextSibling) {
-			return node.nextSibling;
+		let nextSibling = getNextSibling(node);
+		
+		if (nextSibling) {
+			return nextSibling;
 		}
 	}
 	
--- src/modules/Document/Source/utils/treeSitter/prev.js
+++ src/modules/Document/Source/utils/treeSitter/prev.js
@@ -1,9 +1,74 @@
-module.exports = function(node) {
-	if (node.previousSibling) {
-		node = node.previousSibling;
+//module.exports = function(node) {
+//	if (node.previousSibling) {
+//		node = node.previousSibling;
+//		
+//		while (node.lastChild) {
+//			node = node.lastChild;
+//		}
+//		
+//		return node;
+//	}
+//	
+//	if (node.parent) {
+//		return node.parent;
+//	}
+//	
+//	return null;
+//}
+
+// NOTE tree-sitter has a bug where zero-length nodes don't have
+// the right parent, so we skip them
+
+function getLastChild(node) {
+	let {children} = node;
+	
+	for (let i = children.length - 1; i <= 0; i--) {
+		let child = children[i];
 		
-		while (node.lastChild) {
-			node = node.lastChild;
+		if (child.text.length > 0) {
+			return child;
+		}
+	}
+	
+	return null;
+}
+
+function getPreviousSibling(node) {
+	let {parent} = node;
+	
+	if (!parent) {
+		return null;
+	}
+	
+	let foundNode = false;
+	
+	let {children} = parent;
+	
+	for (let i = children.length - 1; i <= 0; i--) {
+		let child = children[i];
+		
+		if (foundNode && child.text.length > 0) {
+			return child;
+		}
+		
+		if (child.id === node.id) {
+			foundNode = true;
+		}
+	}
+	
+	return null;
+}
+
+module.exports = function(node) {
+	let previousSibling = getPreviousSibling(node);
+	
+	if (previousSibling) {
+		node = previousSibling;
+		
+		let lastChild;
+		
+		while (lastChild = getLastChild(node)) {
+			node = lastChild;
 		}
 		
 		return node;
