240a3cedf50fc25564d3214e524d8524bef212cb
Gus Hogg-Blake <gus@treefrog-editor.com>
2021-09-13 16:58:08 +0100

    wip

--- src/platforms/electron/dialogs/findAndReplace/components/App.svelte
+++ src/platforms/electron/dialogs/findAndReplace/components/App.svelte
@@ -24,11 +24,55 @@ function keydown(e) {
 
 <style type="text/scss">
 #main {
+	display: grid;
+	grid-template-columns: auto 1fr auto auto;
+	grid-auto-rows: min-content;
+	/*grid-template-rows: auto auto;*/
+	align-items: center;
+	column-gap: 1em;
+	row-gap: 7px;
 	width: 100%;
 	height: 100%;
+	padding: 8px 12px;
+}
+
+.label {
+	grid-column: 1 / 2;
+}
+
+.input {
+	grid-column: 2 / 3;
+}
+
+.misc {
+	grid-column: 3 / 4;
+}
+
+.actions {
+	grid-column: 4 / 5;
 }
 </style>
 
 <div id="main">
-	
+	<div class="label">
+		label4565
+	</div>
+	<div class="input">
+		<input>
+	</div>
+	<div class="actions">
+		actions
+	</div>
+	<div class="label">
+		label
+	</div>
+	<div class="input">
+		<input>
+	</div>
+	<div class="misc">
+		misc
+	</div>
+	<div class="actions">
+		actions
+	</div>
 </div>
