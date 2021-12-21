46d81806efcddb343bcb72a383ff085cbe36a6a9
Gus Hogg-Blake <gus@treefrog-editor.com>
2021-12-21 16:56:00 +0000

    wip

--- src/platforms/web/main.js
+++ src/platforms/web/main.js
@@ -2,6 +2,7 @@ import Base from "modules/Base";
 import App from "modules/App";
 import components from "components";
 import AppComponent from "components/App/App.svelte";
+import Editor from "components/Editor/Editor.svelte";
 import Platform from "./Platform";
 
 window.platform = new Platform();
@@ -11,19 +12,23 @@ export default async function(options) {
 	await platform.init(options);
 	await base.init(components);
 	
-	return async function(el) {
-		let app = new App();
-		
-		await app.init();
-		
-		new AppComponent({
-			target: el,
+	return {
+		async app(el) {
+			let app = new App();
 			
-			props: {
-				app,
-			},
-		});
+			await app.init();
+			
+			new AppComponent({
+				target: el,
+				
+				props: {
+					app,
+				},
+			});
+			
+			return app;
+		},
 		
-		return app;
-	}
+		Editor,
+	};
 }
