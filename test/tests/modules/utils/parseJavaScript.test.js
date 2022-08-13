7d9b23cd7f88208ef6a8a6dd37b81b157dac3fa5
Gus Hogg-Blake <gus@treefrog-editor.com>
2022-08-13 19:43:09 +0100

    remove old tests

--- test/tests/modules/utils/parseJavaScript.test.js
+++ test/tests/modules/utils/parseJavaScript.test.js
@@ -157,7 +157,7 @@ let tests = [
 describe("parseJavaScript", function() {
 	for (let [name, code, startIndex, expectedEndIndex] of tests) {
 		it(name, function() {
-			let endIndex = parseJavaScript(dedent(code), startIndex);
+			let {index: endIndex} = parseJavaScript(dedent(code), startIndex);
 			
 			is(endIndex, expectedEndIndex);
 		});
